2465ef578cc6980e9edbacada2377df00449c7f9
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-19 12:22:21 +0900

    updaete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import LatestPosts from '@/components/home/latest-posts';
-import MainNav from '@/components/main-nav';
+import { MainNav } from '@/components/main-nav';
 
 export default function Home() {
   return (
--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -18,7 +18,7 @@ const NavigationMenu = React.forwardRef<
     {...props}
   >
     {children}
-    <NavigationMenuViewport />å
+    <NavigationMenuViewport />
   </NavigationMenuPrimitive.Root>
 ));
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;

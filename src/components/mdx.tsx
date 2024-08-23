990ff8cc921afe7b01ff027f474141d948b25fae
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-23 14:51:58 +0900

    update

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
-const CustomMDX = () => {
-  return <div>mdx</div>;
+const createHeading = (level: number) => {};
+
+const components = {
+  h1: createHeading(1),
+};
+
+const CustomMDX = (props: any) => {
+  return (
+    <MDXRemote
+      {...props}
+      components={{ ...components, ...(props.components || {}) }}
+    >
+      mdx
+    </MDXRemote>
+  );
 };
 
 export default CustomMDX;

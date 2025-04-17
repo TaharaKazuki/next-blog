4293e52f1795d93c766e997dc142fb179cdd141e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-04-17 09:43:34 +0900

    投稿検索機能を追加: searchPosts関数を実装し、検索ワードに基づいて投稿をフィルタリング。Prismaを使用して、タイトルまたはコンテンツに一致する投稿を取得し、作成日時でソート。zodを用いて取得した投稿データを検証。

--- lib/post.ts
+++ lib/post.ts
@@ -35,3 +35,27 @@ export const getPost = async (id: string) => {
 
   return ClientPostSchema.parse(post);
 };
+
+export const searchPosts = async (search: string) => {
+  const decodedSearch = decodeURIComponent(search);
+  const normalizedSearch = decodedSearch.replace(/[\s　]+/g, ' ').trim();
+  const searchWords = normalizedSearch.split(' ').filter(Boolean);
+
+  const filters = searchWords.map((word) => ({
+    OR: [{ title: { contains: word } }, { content: { contains: word } }],
+  }));
+
+  const posts = await prisma.post.findMany({
+    where: { AND: filters },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return z.array(ClientPostSchema).parse(posts);
+};

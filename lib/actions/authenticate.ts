fa584797c9e2127bf4085fb161066a5a7023b242
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-04-21 16:11:30 +0900

    投稿ページのUIをさらに改善し、PostCardコンポーネントのスタイルを微調整。レスポンシブデザインの最適化を行い、ユーザーエクスペリエンスを向上させるために、CSSの追加修正を実施。

---
+++ lib/actions/authenticate.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { AuthError } from 'next-auth';
+// eslint-disable-next-line import/order
+import { redirect } from 'next/navigation';
+
+import { signIn } from '@/auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+    redirect('/dashboard');
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
+}

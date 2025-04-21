484d9402cea7ede7ebc4e76088884af7853435e0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-04-21 15:51:13 +0900

    投稿ページのUIを改善し、PostCardコンポーネントのスタイルを調整。レスポンシブデザインを強化し、ユーザーエクスペリエンスを向上させるために、CSSの修正を行いました。

---
+++ auth.ts
@@ -0,0 +1,35 @@
+import bcrypt from 'bcryptjs';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+import { prisma } from '@/lib/prisma';
+
+import { authConfig } from './auth.config';
+
+async function getUser(email: string) {
+  return await prisma.user.findUnique({
+    where: { email: email },
+  });
+}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+        return null;
+      },
+    }),
+  ],
+});
